9abc43201187db4c62ce37f3e55983a1acda8965
sahhebali <saheb.co21@gmail.com>
2021-09-16 23:25:34 +0530

    recup-loop

---
+++ while-loop-exzample.js
@@ -0,0 +1,27 @@
+// loop variable
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+// var trainticet = 1;
+// while (trainticet <= 5) {
+//     console.log(trainticet);
+//     trainticet++
+
+// }
+
+
+var number = 1;
+
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
+}
+
+// even numbers
+// number = 0;
+// while (number <= 20) {
+//     console.log(number);
+//     number = number + 2;
+// }
---
+++ while-loop.js
@@ -0,0 +1,16 @@
+// var rostGiven = 0;
+// while (rostGiven < 7) {
+//     console.log('rost den plese');
+//     rostGiven++;
+//     console.log(rostGiven);
+// }
+// var name = "false";
+// console.log(name);
+
+var rosogollaGiven = 0;
+while (rosogollaGiven < 5) {
+    console.log('rosogolla diben plz');
+    rosogollaGiven++;
+    console.log(rosogollaGiven);
+}
+
